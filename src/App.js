ccbba5d48ce4a57a892667e3849ea71f102c617d
Vinay <kommisettivinay07@gmail.com>
2023-01-25 12:55:19 +0530

    react app week-1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Test from "./Test"
+const app =() => {
+  return (
+    <div>
+    </div>
+  )
+}
+export default app;
---
+++ src/Test.js
@@ -0,0 +1,21 @@
+import React from "react";
+{/*const Test =() => {
+  return (
+    <div>
+// <h1>hiii vinayak</h1>
+    </div>
+)
+} */}
+
+class Test extends React.Component{
+    render () {
+        return (
+            <div>
+            <h1 id="testId">Hello i am vinayak</h1>
+            </div>
+        );  
+    }
+}
+
+
+export default Test;
